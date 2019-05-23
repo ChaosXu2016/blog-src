f44b90d651ef80a3e1459ee71b8cac409db11caa
Xu <chaoxu@newbanker.cn>
2019-05-23 14:17:37 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: '/',
+  title: 'xxx的博客',
+  description: '这是说明这是说明～～～',
+  themeConfig: {
+    sidebar: {
+      '/diary/': [
+        '2019-05-23'
+      ],
+      '/technique/': [
+        'taro-and-weapp'
+      ],
+      '/': [
+        ['technique/', '技术'],
+        ['diary/', '随笔'],
+      ]
+    }
+  }
+}
